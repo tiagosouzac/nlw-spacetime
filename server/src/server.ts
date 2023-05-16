e2394792a6b581820de83beb5b4d6d5dd1e097b5
Tiago Castro <tiagocastro602@gmail.com>
2023-05-15 21:24:14 -0300

    setup server project

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/users', async () => {
+  return await prisma.user.findMany()
+})
+
+app.listen({ port: 3333 }, (err, address) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+
+  console.log(`Server listening at ${address}! 🚀`)
+})
