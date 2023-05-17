696799212d7ddfb7fee0f231dbd8f7d4294cc406
Tiago Castro <tiagocastro602@gmail.com>
2023-05-16 21:19:25 -0300

    config: web - right side of the home page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,21 @@
 export default function Home() {
   return (
-    <>
-      <h1>Hello, World!</h1>
-      <h1 className="font-alt">Hello, World!</h1>
-    </>
+    <main className="grid min-h-screen grid-cols-2">
+      {/* Left */}
+      <div></div>
+
+      {/* Right */}
+      <div className="grid p-16">
+        <div className="flex items-center justify-center">
+          <p className="max-w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a className="underline hover:text-gray-50" href="/">
+              criar agora
+            </a>
+            !
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
