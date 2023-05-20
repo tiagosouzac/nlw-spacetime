8a707722b8ba7f217f26e63508c7e678abab3c60
Tiago Castro <tiagocastro602@gmail.com>
2023-05-20 12:55:30 -0300

    feat: web - github login redirect

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         {/* Sign In */}
         <a
           className="group flex items-center gap-3 text-left transition-colors hover:text-gray-50"
-          href=""
+          href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
         >
           <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
             <User className="h-5 w-5 text-gray-500 transition-colors group-hover:text-gray-100" />
