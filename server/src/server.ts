3435e5c5f0658f25da3818c538b135441364cba3
Tiago Castro <tiagocastro602@gmail.com>
2023-05-20 13:34:24 -0300

    feat: server - auth route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,15 @@
+import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 
 app.register(cors, { origin: true })
+app.register(jwt, { secret: 'spacetime' })
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app.listen({ port: 3333 }, (err, address) => {
