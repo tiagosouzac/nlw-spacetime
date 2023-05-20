ca586b2bad32e8d7efb683b5ab018c75dee3ab9e
Tiago Castro <tiagocastro602@gmail.com>
2023-05-20 16:59:10 -0300

    feat: web - authenticate the user

---
+++ web/src/app/api/auth/callback/route.ts
@@ -0,0 +1,19 @@
+import { api } from '@/lib/api'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url)
+  const code = searchParams.get('code')
+
+  const registerResponse = await api.post('/register', { code })
+
+  const { token } = registerResponse.data
+  const redirectUrl = new URL('/', request.url)
+  const cookieExpirationInSeconds = 60 * 60 * 24 * 30 // 1 month
+
+  return NextResponse.redirect(redirectUrl, {
+    headers: {
+      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpirationInSeconds}`,
+    },
+  })
+}
