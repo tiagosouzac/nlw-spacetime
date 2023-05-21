8236e793f1d34415c4da706b80df0b454941c4fa
Tiago Castro <tiagocastro602@gmail.com>
2023-05-21 16:14:20 -0300

    refactor: web - layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,8 +1,10 @@
 import { ReactNode } from 'react'
+import { cookies } from 'next/headers'
 import {
   Roboto_Flex as Roboto,
   Bai_Jamjuree as BaiJamjuree,
 } from 'next/font/google'
+import { Profile, SignIn, Hero, Copyright } from '@/components'
 import './globals.css'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
@@ -20,12 +22,32 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticated = cookies().get('token')
+
   return (
     <html lang="pt-br">
       <body
         className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2">
+          {/* Left */}
+          <div className="relative grid content-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+            {isAuthenticated ? <Profile /> : <SignIn />}
+            <Hero />
+            <Copyright />
+
+            {/* Blur */}
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+
+            {/* Stripes */}
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+          </div>
+
+          {/* Right */}
+          <div className="grid bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,28 +1,5 @@
-import { cookies } from 'next/headers'
-import { SignIn, Hero, Copyright, EmptyMemories, Profile } from '@/components'
+import { EmptyMemories } from '@/components'
 
 export default function Home() {
-  const isAuthenticated = cookies().get('token')
-
-  return (
-    <main className="grid min-h-screen grid-cols-2">
-      {/* Left */}
-      <div className="relative grid content-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
-        {isAuthenticated ? <Profile /> : <SignIn />}
-        <Hero />
-        <Copyright />
-
-        {/* Blur */}
-        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
-
-        {/* Stripes */}
-        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
-      </div>
-
-      {/* Right */}
-      <div className="grid bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-        <EmptyMemories />
-      </div>
-    </main>
-  )
+  return <EmptyMemories />
 }
