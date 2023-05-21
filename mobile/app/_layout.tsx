585d42aa83af75321de0e4d01a0fcf4d35f9e982
Tiago Castro <tiagocastro602@gmail.com>
2023-05-21 18:34:17 -0300

    feat: mobile - validate user authentication and redirect to memories

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { ImageBackground } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
+import * as SecureStore from 'expo-secure-store'
 import { styled } from 'nativewind'
 
 import blurBg from '../src/assets/bg-blur.png'
@@ -13,10 +14,19 @@ import {
 
 import { BaiJamjuree_700Bold as BaiJamjureeBold } from '@expo-google-fonts/bai-jamjuree'
 import { Stack } from 'expo-router'
+import { useEffect, useState } from 'react'
 
 const StyledStripes = styled(Stripes)
 
-export default function Layout({ children }) {
+export default function Layout() {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState(false)
+
+  useEffect(() => {
+    SecureStore.getItemAsync('token').then((token) => {
+      setIsUserAuthenticated(!!token)
+    })
+  }, [])
+
   const [hasLoadedFonts] = useFonts({
     RobotoRegular,
     RobotoBold,
@@ -42,7 +52,10 @@ export default function Layout({ children }) {
             backgroundColor: 'transparent',
           },
         }}
-      />
+      >
+        <Stack.Screen name="index" redirect={isUserAuthenticated} />
+        <Stack.Screen name="memories" />
+      </Stack>
     </ImageBackground>
   )
 }
